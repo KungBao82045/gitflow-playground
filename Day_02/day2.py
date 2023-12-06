31a76aae158dd29a6e963a85deff1f635a193cdb
Jacky Cao <jacky.cao@gabler.no>
2023-12-06 10:53:12 +0100

    new solution

--- Day_02/day2.py
+++ Day_02/day2.py
@@ -1,17 +1,45 @@
-# Sum up game IDs if the configuration is possible: 12 red cubes, 13 green cubes, and 14 blue cubes
+# Sum up the cubes and see if they don't reach up beyong limits.
+# If valid, sum up the possible IDs
 
-reading = open("Day_02/day2.txt", "r")
-testing = open("Day_02/testing.txt", "r")
+import re
 
-toList = testing.read().split()
+reading = open("Day_02/day2.txt").read()
 
-red, green, blue = 12, 13, 14
 
-for x in toList:
-    print(x, end=" ")
-    if ";" in x:
-        print("www")
-        break
-    if "," in x:
-        print("-")
+reSkills = re.findall("([Game]{4})\s(\d{1,})(:\s)(.+)", reading)
 
+sum_ID = []
+
+color = ["red", "green", "blue"]
+
+for game, id, colon, val in reSkills:
+    check = True
+
+    splitting = val.split(";")
+
+    for x in splitting:
+        y = x.split(",")
+        for z in y:
+
+            c = 0
+            while c < len(color):
+                if color[c] in z:
+                    stripping = z.strip()
+                    splitting = stripping.split(" ")
+                    if color[c] == "red":
+                        if int(splitting[0]) > 12:
+                            check = False
+                    elif color[c] == "green":
+                        if int(splitting[0]) > 13:
+                            check = False
+                    elif color[c] == "blue":
+                        if int(splitting[0]) > 14:
+                            check = False
+                    
+                c += 1
+    if check:
+        # print(f"{id}: {val}")
+        sum_ID.append(id)
+
+answer = [int(sum_ID[y]) for y in range(0, len(sum_ID))]
+print("Final answer for day 2:", sum(answer), "|", type(answer))
